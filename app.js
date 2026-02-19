fd1b765c9e5b3a4a49b25e732cadda29f7a3cc18
jorgecordonesfernandez-max <jorgecordonesfernandez@gmail.com>
2026-02-19 20:10:14 +0100

    Web inicial Limpiezas Icorse

---
+++ app.js
@@ -0,0 +1,15 @@
+﻿(function () {
+  var scrollTrigger = 36;
+
+  function updateHeaderState() {
+    if (window.scrollY > scrollTrigger) {
+      document.body.classList.add('page-scrolled');
+    } else {
+      document.body.classList.remove('page-scrolled');
+    }
+  }
+
+  window.addEventListener('scroll', updateHeaderState, { passive: true });
+  window.addEventListener('resize', updateHeaderState);
+  updateHeaderState();
+})();
